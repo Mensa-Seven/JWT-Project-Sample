6a7c79fae75cc3c3d3f0920ebe0b8baa766a9553
Mensa-Seven <boatbot.mensa@gmail.com>
2022-01-16 02:11:19 +0700

    adding function VerifyToken and adding path login

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const User = require('./model/user')
 const jwt = require("jsonwebtoken")
 const express = require('express')
 const bcrypt = require('bcryptjs/dist/bcrypt')
+const auth = require('./middleware/auth')
+const req = require('express/lib/request')
+const res = require('express/lib/response')
 const app = express()
 
 //กำหนดให้ สามารถรับค่ามาเป็น json 
@@ -47,9 +50,44 @@ app.post('/register', async(req, res) => {
     }catch(err) {
         console.log(err);
     }
-    
-
-
-
 })
+
+app.post('/login', async(req, res) => {
+    try{
+
+        const { email, password } = req.body
+        // validate and check inputs require
+
+        if(!(email && password)){
+            res.status(400)
+            .send('inputs email and password')
+        }
+
+        //validate and check user 
+        const user = await User.findOne({ email:email })
+        if(user && bcrypt.compare(password, user.password)){
+            const token = jwt.sign(
+                
+                {user_id:user._id, email},
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn:"2h"
+                }
+            )
+            //save use token
+            user.token = token
+            res.status(200)
+            .json(user)
+        }
+      
+        res.status(400)
+        .send('Invalid Crentais')
+    }catch(err){
+        console.log(err);
+    }
+})
+app.post('/welcome', auth, (req, res) => {
+    res.status(200).send('Welcome')
+})
+
 module.exports = app;
